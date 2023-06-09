cf988e7424995d682cd9373c51d48b9b44ca8254
AdithNair <104628360+AdithNair@users.noreply.github.com>
2023-06-09 11:16:08 -0400

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+
+    
+    
+    # FOR LOOP
+    list1 = ["Soccer" , "Basketball" , "Cricket" , "Tennis" , "Golf"]
+    print(list1)
+        
+        
+    list2 = ["Music" , "Painting" , "Dance" , "Writing" , "Sketching"]
+    print(list2)
+            
+            
+    list3 = ["Goal" , "Hoop" , "Wicket" , "Net" , "Club"]
+    print(list3)
+    
---
+++ 2.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+root = Tk()
+
+root.title("Fibonacci")
+root.geometry("600x400")
+root.mainloop()
